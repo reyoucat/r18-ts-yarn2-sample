2514cb0d4150573a3387582ed99b275cf030f1c6
reyoucat <re.you.cat@gmail.com>
2022-05-01 13:18:10 +0900

    vite + yarn berry + no pnp

--- jset.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  testPathIgnorePatterns: [ '<rootDir>/node_modules/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
-  },
-};
---
+++ src/StyledButton.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { darken, lighten } from 'polished';
+
+const StyledButton = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* 크기 */
+  height: 2.25rem;
+  font-size: 1rem;
+
+  /* 색상 */
+  background: #228be6;
+  &:hover {
+    background: ${lighten(0.1, '#228be6')};
+  }
+  &:active {
+    background: ${darken(0.1, '#228be6')};
+  }
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+export default StyledButton;
